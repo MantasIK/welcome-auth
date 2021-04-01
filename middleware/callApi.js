f0052241580231923baa8eab4fb30ebacf8bcf18
Mantas Ivaskevicius <ivaskeviciusmantas@gmail.com>
2021-03-31 20:31:11 -0400

    env don't work on next.js like expected. Hardcoding url

--- middleware/callApi.js
+++ middleware/callApi.js
@@ -1,10 +1,10 @@
-const server_url = process.env.SERVER_URL
-  ? process.env.SERVER_URL
-  : "http://localhost:3000";
+// const url = `${process.env.SERVER_URL}/api/userData`;
+//screw next.js
 
 export const postData = async (data) => {
+  console.log(process.env.SERVER_URL);
   try {
-    const res = await fetch(`${server_url}/api/userData`, {
+    const res = await fetch(`https://welcome-auth.vercel.app/api/userData`, {
       method: "post",
       body: JSON.stringify(data),
     });
